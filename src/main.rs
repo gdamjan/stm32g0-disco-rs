f22c5265dc54a6d67865388f6a0933d269363846
Damjan Georgievski <gdamjan@gmail.com>
2020-02-05 18:02:21 +0100

    commit the blinky code

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![deny(warnings)]
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+// #[allow(unused)]
+// use panic_halt;
+
+#[allow(unused)]
+use panic_semihosting;
+
+use cortex_m_rt::entry;
+use stm32g0xx_hal as hal;
+
+use hal::prelude::*;
+use hal::rcc::Config;
+use hal::stm32;
+
+#[entry]
+fn main() -> ! {
+    let dp = stm32::Peripherals::take().expect("cannot take peripherals");
+    let mut rcc = dp.RCC.freeze(Config::lsi());
+    let mut delay = dp.TIM16.delay(&mut rcc);
+
+    let gpioa = dp.GPIOA.split(&mut rcc);
+    let mut led = gpioa.pa12.into_push_pull_output();
+
+    loop {
+        led.toggle().unwrap();
+        delay.delay(500.ms());
+    }
+}
